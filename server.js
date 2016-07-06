fc6467d6319791f30ab97b40121830e6dfcd657c
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-07-06 11:31:46 -0400

    npm dependencies and server.js parse, port, etc.

---
+++ server.js
@@ -0,0 +1,24 @@
+//dependencies
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var app = express();
+var expressHandlebars = require('express-handlebars');
+var connection = require('./config/connection.js');
+var path = require('path');
+
+//static content from the "public" directory
+app.use(express.static(__dirname + '/public'));
+
+//parse application
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public/assets'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+require('./controllers/burgersController.js')(app);
+
+var port = 3001;
+app.listen(port, function() {
+    console.log("Listening on PORT " + port);
+});
