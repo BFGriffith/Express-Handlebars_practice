17fa94d844a10d1a389b3a98f4e6e30a7a887d39
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-07-06 11:58:28 -0400

    initial scaffolding/setup

--- server.js
+++ server.js
@@ -11,14 +11,18 @@ var path = require('path');
 app.use(express.static(__dirname + '/public'));
 
 //parse application
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static('public/assets'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 require('./controllers/burgersController.js')(app);
 
 var port = 3001;
 app.listen(port, function() {
-    console.log("Listening on PORT " + port);
+  console.log("Listening on PORT " + port);
 });
