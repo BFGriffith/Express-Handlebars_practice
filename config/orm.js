63add28b999bbe783791c71eb797dcc8b6ffeb12
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-07-09 08:49:35 -0400

    working on Promise structure, but still have an unresolved path error somewhere

--- config/orm.js
+++ config/orm.js
@@ -1,72 +1,33 @@
-var connection = require('./config/connection.js');
+var connection = require('./connection.js');
 
 var orm = {
-selectAll: function(table){
-        return new Promise(function(resolve, reject){
-            var queryString = 'SELECT * FROM '+ table +'';
-            connection.query(queryString, function(err, res){
-                if (err) throw err;
-                //console.log(res);
-                return resolve(res);
-            });
-        })
-    },
-    app.get('/', function(req,res) {
-        orm.selectAll('burgers').then(function(data){
-            //console.log(data);
-            res.render('index', {burgers: data,
-                helpers: {isAte: function(burger){
-                //console.log(burger);
-                    if (burger.devoured === 1) {
-                        return burger.id+'. '+burger.burgerName;
-                    }else{
-                        return ;
-                    }
-                }},
-            });
-        })
-    });
-};//END orm.js
-/*
-var orm = {
-  selectAll: function(tableInput, columnToSearch, valueOfColumn) {
+  selectAll: function(table) {
     return new Promise(function(resolve, reject) {
-      var queyString = 'SELECT * FROM ' + burgers + ' WHERE ' + burgerName + ' = ?';
-      connection.query(queryString, [valueOfColumn], function(err, result) {
-        resolve(result);
+      var queryString = 'SELECT * FROM ' + table + '';
+      connection.query(queryString, function(err, res) {
+        if (err) throw err;
+        return resolve(res);
       });
-    });
-  }
-};
-*/
-
-/*
-var orm = {
-  selectAll: function(table, column, columnValue) {
-    var queryString = 'SELECT * FROM ' + table + ' WHERE ' + column + ' = ?';
-    connection.query(queryString, [columnValue], function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      return res;
     })
   },
-  insertOne: function(table, column1, column2, column3, columnOneValue) {
-    var queryString = 'INSERT INTO ' + table + ' (' + column1 + ', ' + column2 + ', ' + column3 + ') VALUES (?,?,?)';
-    connection.query(queryString, [columnOneValue, false, now()], function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      return res;
+  insertOne: function(table, newObject) {
+    return new Promise(function(resolve, reject) {
+      var queryString = 'INSERT INTO ' + table + ' SET ?';
+      connection.query(queryString, newObject, function(err, res) {
+        if (err) throw err;
+        return resolve(res);
+      });
     });
   },
   updateOne: function(table, column1, column2, columnTwoValue) {
-    var queryString = 'UPDATE ' + table + ' SET ' + column1 + ' = ? WHERE ' + column2 + ' = ?';
-    connection.query(queryString, [true, columnTwoValue], function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      return res;
+    return new Promise(function(resolve, reject) {
+      var queryString = 'UPDATE ' + table + ' SET ' + column1 + ' = ? WHERE ' + column2 + ' = ?';
+      connection.query(queryString, [true, columnTwoValue], function(err, res) {
+        if (err) throw err;
+        return resolve(res);
+      });
     });
   }
-};
-*/
+};//END orm
 
 module.exports = orm;
--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,62 +1,48 @@
-//dependencies
+//dependencies (not sure if I need all these?)
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
+var html = require('../views/index.handlebars');
 var burger = require('../models/burger.js');
+var orm = require('../config/orm.js');
 var path = require('path');
 var app = express();
 
+/*ROUTES*/
 module.exports = function(app) {
+
   app.get('/', function(req, res) {
-    res.render('index', {})
+    orm.selectAll('burgers').then(function(data) {
+      res.render('index', {
+        burgers: data,
+        helpers: {
+          isEaten: function(burger) {
+            if (burger.devoured === 1) {
+              return "#" + burger.id + ' ' + burger.burgerName;
+            } else {
+              return;
+            }
+          }
+        },
+      });
+    })
   });
-  app.post('/create', function(req, res) {})
+
+  app.post('/create', function(req, res) {
+    orm.insertOne('burgers', {
+      burger_name: req.body.burger,
+      devoured: false
+    }).then(function(data) {});
+  });
+
+  app.get("/newBurger", function(req, res) {
+    orm.selectAll('burgers').then(function(data) {
+      res.send(data);
+    })
+  });
+
+  app.get('/eatBurger', function(req, res) {
+    orm.updateOne('burgers', 'devoured', 'id', req.body.id).then(function(data) {});
+  });
 };
-
-/*
-module.exports = function(app){
-
-app.get('/', function (req, res) {
-    connection.query('SELECT * FROM burgers', function(err, result) {
-        if (err) throw err;
-        res.render('index', {
-            burgers: result
-        });//END res.render object
-    });//END connection.query
-});//END app.get
-
-app.get('/index', function (req, res) {
-    connection.query('SELECT * FROM burgers WHERE devoured = false', function(err, result) {
-        if (err) throw err;
-        res.render('index', {
-            burgers: result
-        });//END res.render object
-    });//END connection.query
-
-    connection.query('SELECT * FROM burgers WHERE devoured = true', function(err, result) {
-        if (err) throw err;
-        res.render('index', {
-            burgers: result
-        });//END res.render object
-    });//END connection.query
-});//END app.get
-
-
-
-app.get('/devour/:identity', function (req, res) {
-    connection.query('SELECT * FROM burgers WHERE id = identity', function(err, result) {
-        if (err) throw err;
-        res.render('index', {
-            quotes: result
-        });//END res.render object
-    });//END connection.query
-});//END app.get
-
-	//default home
-	app.use(function(req, res){
-		res.sendFile(path.join(__dirname + '/../views/index.handlebars'));
-	});
-
-}
-*/
